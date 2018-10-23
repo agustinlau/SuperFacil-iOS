c0e29153f5873a95c4c2e75ce31caaced365df19
alau7 <alau7@gatech.edu>
2018-10-22 21:10:59 -0700

    Add navigation controller and connect home screen to product list screen

--- SuperFacil/ViewController.swift
+++ SuperFacil/ViewController.swift
@@ -13,6 +13,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let produceButton = UIButton()
+        let produceImage = UIImage(named: "produce") as UIImage?
+        produceButton.frame = CGRect(x: 100, y: 100, width: 100, height: 100)
+        produceButton.setImage(produceImage, for: .normal)
+        produceButton.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        self.view.addSubview(produceButton)
+    }
+    
+    @objc func buttonAction(sender: UIButton!) {
+        print("Button tapped")
+        let view = ProductListViewController()
+//        view.category = "test"
+        self.navigationController?.pushViewController(view, animated: true)
+        
     }
 
 
