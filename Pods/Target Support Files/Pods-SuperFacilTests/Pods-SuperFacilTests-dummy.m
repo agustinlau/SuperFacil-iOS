34615d2767865ebf8a785c19112841c586f77684
Esteban Lau <elau7@gatech.edu>
2018-10-22 21:16:15 -0700

    Created Firebase connection with CocoaPods

---
+++ Pods/Target Support Files/Pods-SuperFacilTests/Pods-SuperFacilTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SuperFacilTests : NSObject
+@end
+@implementation PodsDummy_Pods_SuperFacilTests
+@end
---
+++ Pods/Target Support Files/Pods-SuperFacilUITests/Pods-SuperFacilUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SuperFacilUITests : NSObject
+@end
+@implementation PodsDummy_Pods_SuperFacilUITests
+@end
